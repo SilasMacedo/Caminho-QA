e60d3e782df86fe63653aa875751213bf9a72bba
Silas Macedo <arqvimedes@gmail.com>
2021-10-19 09:37:24 -0300

    variaveis

---
+++ Java/java.git/src/main/java/variaveis/Variaveis.java
@@ -0,0 +1,18 @@
+package variaveis;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int numeroInteiro = 3;
+		double numeroQuebrado = 1.5;
+		boolean verdadeiroFalso = false;
+				
+		numeroInteiro = 10;
+		System.out.println("O numero é: " +numeroInteiro);
+	
+		System.out.println("A variavel do tipo inteiro pode receber " +numeroInteiro);
+		System.out.println("A variavel do tipo double é " +numeroQuebrado);
+		System.out.println("Variavel do tipo boolean pode receber " +verdadeiroFalso);
+	}
+}
